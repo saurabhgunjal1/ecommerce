8c4b74aa4fbb9e462ee2f891ab7cde2d1fc0fdae
Saurabh Gunjal <gunjalsaurabh16@gmail.com>
2024-08-26 00:08:41 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Navbar from "./components/navbar/navbar";
 import Herosection from "./components/herosection/herosection";
 import Filterbar from "./components/filterbar/filterbar";
 import Products from "./components/products/products";
-import footer from "./assets/footer.png";
+import footer from "./assets/Footer.png";
 
 function App() {
   const [hideFilter, setHideFilter] = useState("flex");
